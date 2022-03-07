e3ee2789888b84c73d7b61409b7edf6e23fe687d
ryant02 <totoryan69@gmail.com>
2022-03-07 15:39:29 +0000

    Update game.py
    wrote basic code to display a moving circle on the screen

--- game.py
+++ game.py
@@ -1,3 +1,72 @@
-Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
+#------------------------------------
+# NPA Game Project
+# Coded by Ryan To
+#------------------------------------
+
+#------------------------------------
+# import libraries
+#------------------------------------
+import pygame
+#------------------------------------
+
+
+#------------------------------------
+# initialisation and setup
+#------------------------------------
+pygame.init()
+
+# set up the window we'll use to draw our game
+screen = pygame.display.set_mode([500,500])
+
+
+# set long term variables
+running = True
+circle_pos = 250, 250
+#------------------------------------
+
+
+
+#------------------------------------------------------------------------
+# game loop
+#------------------------------------------------------------------------
+# run the loop forever until the user asks to quit
+while running:
+
+
+    #----------------------------------------------------------------------
+    # Input
+    # check if the user clicked the close button
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                circle_pos = (circle_pos[0]-10, circle_pos[1])
+            if event.key == pygame.K_RIGHT:
+                circle_pos = (circle_pos[0]+10, circle_pos[1])
+            if event.key == pygame.K_UP:
+                circle_pos = (circle_pos[0], circle_pos[1]-10)
+            if event.key == pygame.K_DOWN:
+                circle_pos = (circle_pos[0], circle_pos[1]+10)
+            
+            
+        # and if keydown    
+    #----------------------------------------------
+
+    #----------------------------------------------
+    # Update
+    #----------------------------------------------
+    
+    #----------------------------------------------
+
+
+    #----------------------------------------------
+    # Draw
+    #----------------------------------------------
+    # fill background with colour
+    screen.fill((255, 255, 255))
+
+    pygame.draw.circle(screen, (125, 125, 125), (circle_pos), 75)
+    
+    pygame.display.flip()
